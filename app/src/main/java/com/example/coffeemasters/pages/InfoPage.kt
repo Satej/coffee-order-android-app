fc665700482c26c55d0cdd70697f05acb455fb72
Satej <Satej>
2024-06-28 19:57:34 +0000

    Final

--- app/src/main/java/com/example/coffeemasters/pages/InfoPage.kt
+++ app/src/main/java/com/example/coffeemasters/pages/InfoPage.kt
@@ -1,10 +1,35 @@
 package com.example.coffeemasters.pages
 
+import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
+
+@Composable
+fun MyWebView() {
+    var url = "https://firtman.github.io/coffeemasters/webapp"
+    AndroidView(factory = {
+        WebView(it).apply {
+            layoutParams = ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT,
+            )
+            webViewClient = WebViewClient()
+            loadUrl(url)
+        }
+    }, update = {
+        it.loadUrl(url)
+    })
+}
 
 @Preview
 @Composable
-fun InfoPage() {
-    
+fun InfoPage(modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        MyWebView()
+    }
 }
